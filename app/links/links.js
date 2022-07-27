2a8aaeff0990a5a8b08f073a628ebf0f4600154a
Muhammad Qazafi Sheikh <qazafi.sheikh121@gmail.com>
2022-07-27 21:12:34 +0500

    Delete links.js

--- app/links/links.js
+++
@@ -1,5 +0,0 @@
-export const link = 'http://77.68.127.235:5000/'
-
-export const vendors = link + 'customer/getLocations'
-export const vendorlist = link + 'item/getItemsByVendor?vendorId='
-export const eventV = link + 'customer/getVendorsForEvent?eventId='
